8a519e8d5209713133504c961e8e7458233c7162
4501104220 <thangtrantt@hotmail.com>
2021-05-27 11:08:14 +0700

    edit file adminview.php second times

--- blogadmin/adminview.php
+++ blogadmin/adminview.php
@@ -84,33 +84,40 @@
 
 
 <!--      NOT WORKING -->
-      <!--    <div class="col-lg-3">
-            <div class="panel panel-primary">
-              <div class="panel-heading">
-                <div class="row">
-                  <div class="col-xs-6">
-                    <i class="fa fa-bar-chart-o fa-5x"></i>
-                  </div>
-                  <div class="col-xs-6 text-right">
-                    <p class="announcement-heading"><?php /*adminstats("visitor_info","page_hits");*/?></p>
-                    <p class="announcement-text"><strong>Admin Stats</strong></p>
-                  </div>
-                </div>
-              </div>
-              <a href="../adminstats">
-                <div class="panel-footer announcement-bottom">
-                  <div class="row">
-                    <div class="col-xs-6">
-                      View
-                    </div>
-                    <div class="col-xs-6 text-right">
-                      <i class="fa fa-arrow-circle-right"></i>
+     <!--      <div class="col-lg-3">
+                <div class="panel panel-primary">
+                  <div class="panel-heading">
+                    <div class="row">
+                      <div class="col-xs-6">
+                        <i class="fa fa-bar-chart-o fa-5x"></i>
+                      </div>
+                      <div class="col-xs-6 text-right">
+                        <p class="announcement-heading">
+                            <?php
+//                            /*/*adminstats("visitor_info","page_hits");*/*/
+                            ?>
+                        </p>
+                        <p class="announcement-text"><strong>Admin Stats</strong></p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div><!-- /.row -->
 
-    <!-- NOT WORKING -->
+                  <a href="../adminstats">
+                    <div class="panel-footer announcement-bottom">
+                      <div class="row">
+                        <div class="col-xs-6">
+                          View
+                        </div>
+                        <div class="col-xs-6 text-right">
+                          <i class="fa fa-arrow-circle-right"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+          </div>-->
+     <!-- NOT WORKING -->
+
+
+ </div> <!-- /.row -->
+
